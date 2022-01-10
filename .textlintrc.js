774331a437187f0d37092788e73a1e51f925b61b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-01-10 12:49:13 +0900

    🔧: 各種lintの設定ファイル追加

---
+++ .textlintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  filters: {
+    comments: true,
+  },
+  plugins: {
+    '@textlint/markdown': {
+      extensions: ['.mdx'],
+    },
+  },
+  rules: {
+    // https://github.com/textlint-ja/textlint-rule-preset-ja-technical-writing
+    'preset-ja-technical-writing': {
+      'sentence-length': {
+        max: 128,
+        exclusionPatterns: ['/\\(.*\\)/'],
+      },
+      'ja-no-mixed-period': {
+        allowPeriodMarks: [':::'],
+      },
+      'max-kanji-continuous-len': {
+        max: 6,
+      },
+    },
+    'preset-jtf-style': {
+      '2.2.1.ひらがなと漢字の使い分け': true,
+    },
+    'textlint-rule-preset-ja-spacing': true,
+  },
+};
