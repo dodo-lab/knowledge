3d20a0494a68366093633df867929a82fcff73a6
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-05 22:17:29 +0900

    🔧: 編集機能を無効化

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,8 +25,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
